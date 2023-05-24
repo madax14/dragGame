a4bd07fb4155df184ae4483579edd0a7633b4a4b
Diego <santos.sud@hotmail.com>
2023-05-24 19:19:09 -0300

    Storing localData

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const draggbles = document.querySelectorAll(".dragMe");
 const dropZone = document.querySelectorAll(".dropZone");
-console.log(draggbles)
+// console.log(draggbles)
 // console.log(dropZone)
 
 draggbles.forEach(drag => {
     drag.addEventListener("dragstart", (event) =>{
-        // console.log(event)
         event.dataTransfer.setData("text/plain", event.target.id)
     });
 });
@@ -16,12 +15,28 @@ dropZone.forEach(zone => {
     });
     zone.addEventListener("drop", (event)=>{
         event.preventDefault();
-        console.log(event)
-        const draggbleId = event.dataTransfer.getData("text/plain")
-        const draggbleElement = document.getElementById(draggbleId);
-        
-        zone.appendChild(draggbleElement)
-    })
+        const draggedId = event.dataTransfer.getData("text/plain")
+        const draggedElement = document.getElementById(draggedId);
+        //To append the zone div to the img dragged
+        const appended = zone.appendChild(draggedElement);
+
+        //Strore the position of the dragged img
+        const position = appended.getBoundingClientRect();
+        const positionToStore = [{ left: position.left, top: position.top }]
+        localStorage.setItem(draggedId, JSON.stringify(positionToStore))
+
+        return appended;
+    });
+
+   ///////Retore the position on refresh page//////
+    const draggedId = zone.firstChild?.id;
+    const storedPosition = localStorage.getItem(draggedId);
+    if (storedPosition) {
+        const position = JSON.parse(storedPosition);
+        zone.firstChild.style.left = position.left + "px";
+        zone.firstChild.style.top = position.top + "px";
+    }
+    
 });
 
 
